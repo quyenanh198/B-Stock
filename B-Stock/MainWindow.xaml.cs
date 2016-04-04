892e9c663262fe7f48cb6daa5c70674e3ecb67b6
Quyen Anh <quyenanh198@gmail.com>
2016-04-04 16:45:34 -0700

    Add code

---
+++ B-Stock/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using DocumentFormat.OpenXml;
+using ClosedXML;
+using ClosedXML.Excel;
+
+namespace B_Stock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection server_cn = new SqlConnection(B_Stock.Properties.Settings.Default.strConn);
+
+        Units UnitData;
+        SQLFunc func;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            func = new SQLFunc();
+            UnitData = new Units();
+
+            tbAPN.Focus();
+        }
+
+        
+        private void CreateExcel(DataTable dt,string WorksheetName)
+        {
+            XLWorkbook wb = new XLWorkbook();
+            wb.Worksheets.Add(dt, WorksheetName);
+        }
+        private void PrintLabel()
+        {
+            string Region = (UnitData.APN.Remove(UnitData.APN.Length - 2)).Remove(0,5);
+
+            switch(Region)
+            {
+                case "LL":
+
+                    break;
+                case "B":
+
+                    break;
+                case "Y":
+
+                    break;
+                case "D":
+
+                    break;
+                case "T":
+
+                    break;
+            }
+        }
+        private void tbAPN_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                tbSN.Focus();
+                UnitData.APN = tbAPN.Text;
+            }
+            else
+                return;
+        }
+        private void tbSN_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                UnitData.SN = tbSN.Text;
+                func.GetSNData(UnitData);
+
+                tbTKNo.Text = UnitData.TKNo.ToString();
+                tbRMA_No.Text = UnitData.RMANo;
+                tbRMA_SN.Text = UnitData.RMASN;
+                tbSYSSN.Text = UnitData.SYSSN;
+                tbCONFIG.Text = UnitData.CONFIG;
+                tbFamilyNo.Text = UnitData.FamilyNo;
+                tbUPC.Text = UnitData.UPC;
+                tbModel.Text = UnitData.Model;
+                tbCustPN.Text = UnitData.CustPN;
+                //PrintLabel();
+				
+				//if(func.checkStatus(UnitData))
+				//{
+                    if(func.IsFileExists(UnitData))
+                        MessageBox.Show("Ode");
+                    else
+                        MessageBox.Show("Eo Ode");
+    //            }
+				//else
+				//	MessageBox.Show("Eo Ode");
+            }
+            else
+                return;
+        }
+        private void tbAPN_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void tbSN_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //if (tbSN.Text.Length == 12)
+            //{
+            //    UnitData.SN = tbSN.Text;
+            //    SQLFunc func = new SQLFunc();
+            //    func.GetSNData(UnitData);
+            //    tbTKNo.Text = UnitData.TKNo.ToString();
+            //}
+            //else
+            //    return;
+
+        }
+        private void btExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void btClear_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (TextBox tb in FindVisualChildren<TextBox>(this))
+            {
+                tb.Clear();
+            }
+            UnitData = new Units();
+
+            tbAPN.Focus();
+        }
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+    public class Units
+    {
+        public string SN        { get; set; }
+        public string APN       { get; set; }
+        public string UPC       { get; set; }
+        public string TKNo      { get; set; }
+        public string RMANo     { get; set; }
+        public string RMASN     { get; set; }
+        public string SYSSN     { get; set; }
+        public string Model     { get; set; }
+        public string CONFIG    { get; set; }
+        public string BT_Mac    { get; set; }
+        public string AirMac    { get; set; }
+        public string CustPN    { get; set; }
+        public string MLB_SN    { get; set; }
+        public string Region    { get; set; }
+        public string EtherMac  { get; set; }
+        public string FamilyNo  { get; set; }
+        public string SizeType  { get; set; }
+        public string SIPROM_MAC { get; set; }
+        public string Flag      { get; set; }
+        public bool   flgStatus { get; set; }
+        public bool   flgDW     { get; set; }
+
+    }
+}
---
+++ B-Stock/SQLFunc.cs
@@ -0,0 +1,129 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.Permissions;
+using System.Windows;
+
+namespace B_Stock
+{
+    class SQLFunc
+    {
+        SqlConnection server_cn = new SqlConnection(B_Stock.Properties.Settings.Default.strConn);
+
+        private static SqlDataAdapter SDA;
+        private static DataSet DS;
+        //Units UnitData;
+        string[] txtList;
+
+        public SQLFunc()//Units tmpData)
+        {
+            //UnitData = tmpData;
+        }
+
+        public bool GetSNData(Units tmpData)//string tmpSN, string tmpAPN)
+        {
+            using (SqlConnection cn = new SqlConnection(B_Stock.Properties.Settings.Default.strConn))
+            {
+                cn.Open();
+
+                using (SqlCommand QryCmd = new SqlCommand())
+                {
+                    QryCmd.CommandText = " SELECT A.NewQTA_SN, A.QTA_SN, A.RMACaseID, A.TKNo, A.CustPN, A.QTAModel, B.* " +
+                                         " FROM datRMAEquipments A, PALRMA_MODEL_CONFIG_TE B " +
+                                         " WHERE (A.QTA_SN = '" + tmpData.SN + "' OR A.NewQTA_SN = '" + tmpData.SN + "') " +
+                                         " AND A.CustPN = '" + tmpData.APN + "' " +
+                                         " AND A.DefectivePart = 'System' " +
+                                         " AND A.CustPN = B.APN ";
+                    SDA = new SqlDataAdapter(QryCmd.CommandText, cn);
+                    DS = new DataSet();
+                    SDA.Fill(DS, "UNITS");
+                    if (DS.Tables["UNITS"].Rows.Count > 0)
+                    {
+                        tmpData.RMASN = DS.Tables["UNITS"].Rows[0]["NewQTA_SN"].ToString();
+                        tmpData.SYSSN = DS.Tables["UNITS"].Rows[0]["QTA_SN"].ToString();
+                        tmpData.TKNo = DS.Tables["UNITS"].Rows[0]["TKNo"].ToString();
+                        tmpData.RMANo = DS.Tables["UNITS"].Rows[0]["RMACaseID"].ToString();
+                        tmpData.Model = DS.Tables["UNITS"].Rows[0]["QTAModel"].ToString();
+                        tmpData.CustPN = DS.Tables["UNITS"].Rows[0]["CustPN"].ToString();
+                        tmpData.CONFIG = DS.Tables["UNITS"].Rows[0]["CONFIG"].ToString();
+                        tmpData.UPC = DS.Tables["UNITS"].Rows[0]["UPC"].ToString();
+                        tmpData.FamilyNo = DS.Tables["UNITS"].Rows[0]["FAMILY"].ToString();
+
+                        cn.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        cn.Close();
+                        return false;
+                    }
+                }
+            }
+            
+        }
+        public bool checkStatus(Units tmpData)
+        {
+            using (SqlConnection cn = new SqlConnection(B_Stock.Properties.Settings.Default.strConn))
+            {
+                cn.Open();
+
+                using (SqlCommand QryCmd = new SqlCommand())
+                {
+                    QryCmd.CommandText = " SELECT TOP 1 * " +
+                                         " FROM logEquipmentProcFlow " +
+                                         " WHERE RMACaseID + TKNo = '" + tmpData.RMANo + tmpData.TKNo + "' ";// +
+                                         //" AND Station = 'QC003' ";
+                    SDA = new SqlDataAdapter(QryCmd.CommandText, cn);
+                    DS = new DataSet();
+                    SDA.Fill(DS, "UNITSTATUS");
+					
+                    if (DS.Tables["UNITSTATUS"].Rows.Count > 0)
+                    {
+						if(DS.Tables["UNITSTATUS"].Rows[0]["NextStation"].ToString() == "PACKING")
+						{
+							tmpData.flgStatus = true;
+							cn.Close();
+							return true;
+						}
+						else
+						{
+							tmpData.flgStatus = false;
+							cn.Close();
+							return false;
+						}
+                    }
+                    else
+                    {
+						tmpData.flgStatus = false;
+                        cn.Close();
+                        return false;
+                    }
+                }
+            }
+            //return false;
+        }
+        public bool IsFileExists(Units tmpData)
+        {
+            string SN = tmpData.RMASN.Substring(0, 11) + "*.*";
+            try
+            {
+                FileIOPermission f2;
+                if (File.Exists(@"I:\" + tmpData.RMASN))
+                {
+                    //txtList = Directory.GetFiles(@"C:\Users\quyen\Desktop\test\", "*.*", SearchOption.AllDirectories);
+                    //foreach (string file in txtList)
+                    //{
+                    File.Copy(@"I:\" + tmpData.RMASN, @"\\192.168.89.208\Log\" + tmpData.RMASN);
+                    //File.Copy(@"I:\" + tmpData.RMASN, @"C:\Users\quyen\Desktop\dest\" + tmpData.RMASN);
+                    //}
+                }
+                return true;
+            }
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                MessageBox.Show(dirNotFound.Message);
+            }
+            return false;
+        }
+    }
+}
